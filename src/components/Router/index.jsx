8788eb4f95243c77ff38922ca176546ac9f783d3
Sin9k <blue.sin9k@gmail.com>
2020-01-18 19:50:29 +0300

    run eject

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import AppBar from "../../components/AppBar";
+import Home from "~/pages/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <AppBar />
+          <Home />
         </Route>
       </Switch>
     </BrowserRouter>
--- src/pages/Home/components/HomePage/index.jsx
+++ src/pages/Home/components/HomePage/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import AppBar from "~/components/AppBar";
+
 const HomePage = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <AppBar />
+      <h1>Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
